17f0d937e0817fbb7c14955f1b1b56a6e8a0b199
duygu-duran <dduran@technologica.com>
2024-01-10 21:50:59 +0200

    Added Review model and DTO

---
+++ movie-library/movie-library/src/main/java/tu/movielibrary/movielibrary/dto/ReviewDTO.java
@@ -0,0 +1,16 @@
+package tu.movielibrary.movielibrary.dto;
+
+import java.util.List;
+
+public record ReviewDTO(
+        Long reviewId,
+        String content,
+        Integer movieRating,
+        Long dateTimeMilli,
+        Integer likes,
+        List<String> comments,
+        Long userId,
+        Long movieId,
+        String dateTimeString
+) {
+}
